1251ffec27c5b65c942da6ff60b415bf02b147b0
AniaLaz <lazarenko.ania.sg@gmail.com>
2022-09-30 17:12:06 +0300

    2_time

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+console.log(Player);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+player.on('timeupdate', function(data) {
+//    console.log(data.seconds);
+const time =  data.seconds;
+localStorage.setItem("videoplayer-current-time",JSON.stringify(time));
+});
+
+const time = JSON.parse(localStorage.getItem("videoplayer-current-time"));
+console.log(time);
+player.setCurrentTime(time)
