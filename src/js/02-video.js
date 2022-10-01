05a587639b549a335820472674e87fff672fa639
AniaLaz <lazarenko.ania.sg@gmail.com>
2022-10-01 11:00:33 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,45 @@
+// import Player from '@vimeo/player';
+// console.log(Player);
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+
+// player.on('timeupdate', function(data) {
+// //    console.log(data.seconds);
+// const time =  data.seconds;
+// localStorage.setItem("videoplayer-current-time",JSON.stringify(time));
+// });
+
+// const time = JSON.parse(localStorage.getItem("videoplayer-current-time"));
+// console.log(time);
+// player.setCurrentTime(time)
+
+
+
+
+
 import Player from '@vimeo/player';
-console.log(Player);
+import throttle from "lodash.throttle"
+
+
+console.log(throttle);
+// console.log(Player);
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', function(data) {
-//    console.log(data.seconds);
-const time =  data.seconds;
-localStorage.setItem("videoplayer-current-time",JSON.stringify(time));
-});
+player.on('timeupdate', throttle(addTime,1000));
 
-const time = JSON.parse(localStorage.getItem("videoplayer-current-time"));
-console.log(time);
-player.setCurrentTime(time)
+function addTime(data) {
+    //    console.log(data.seconds);
+    const time =  data.seconds;
+    localStorage.setItem("videoplayer-current-time",JSON.stringify(time));
+    console.log(time);
+   
+    }
+
+
+    try{
+        const timeReturn = JSON.parse(localStorage.getItem("videoplayer-current-time"));
+        console.log(timeReturn);
+        player.setCurrentTime(timeReturn)}
+    catch(error){
+        }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector("form");
+console.log(formEl);
+
+
+formEl.addEventListener('input',onFeedback)
+
+function onFeedback(event) {
+    event.preventDefault();
+    // console.log(`${event.target.name}: ${event.target.value}`)
+const name = event.target.name;
+const value = event.target.value;
+const elements = `${event.target.name}: ${event.target.value}`; 
+
+console.log({elements});
+localStorage.setItem("feedback-form-state",JSON.stringify({elements}
+    ))
+
+};
 
