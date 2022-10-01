b13e454c02eafc82ccef406bfce35f51a24d63c6
AniaLaz <lazarenko.ania.sg@gmail.com>
2022-10-01 11:26:18 +0300

    task3-1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,13 +7,15 @@ formEl.addEventListener('input',onFeedback)
 function onFeedback(event) {
     event.preventDefault();
     // console.log(`${event.target.name}: ${event.target.value}`)
-const name = event.target.name;
-const value = event.target.value;
-const elements = `${event.target.name}: ${event.target.value}`; 
-
-console.log({elements});
-localStorage.setItem("feedback-form-state",JSON.stringify({elements}
-    ))
-
+// const name = event.target.name;
+// const value = event.target.value;
+// const elements = `${event.target.name}: ${event.target.value}`; 
+const {email,message} = event.currentTarget.elements;
+const feedback = {
+    email : email.value,
+    message : message.value
+}
+console.log(feedback);
+localStorage.setItem("feedback-form-state",JSON.stringify(feedback))
 };
 
