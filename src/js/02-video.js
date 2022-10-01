0a95609fda1edb60cb9a0ed40f3cec0cc9a0e7ed
AniaLaz <lazarenko.ania.sg@gmail.com>
2022-10-01 17:05:57 +0300

    task3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,22 +1,3 @@
-// import Player from '@vimeo/player';
-// console.log(Player);
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
-
-// player.on('timeupdate', function(data) {
-// //    console.log(data.seconds);
-// const time =  data.seconds;
-// localStorage.setItem("videoplayer-current-time",JSON.stringify(time));
-// });
-
-// const time = JSON.parse(localStorage.getItem("videoplayer-current-time"));
-// console.log(time);
-// player.setCurrentTime(time)
-
-
-
-
-
 import Player from '@vimeo/player';
 import throttle from "lodash.throttle"
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,24 +1,31 @@
+import throttle from "lodash.throttle"
+console.log(throttle);
+
 const formEl = document.querySelector("form");
 const emailEl = document.querySelector("input");
 const textareaEl = document.querySelector("textarea");
 
-console.log("textareaEl", textareaEl.value);
-console.log(formEl);
 
-formEl.addEventListener('input',onFeedback)
+formEl.addEventListener('input', onFeedback)
 formEl.addEventListener('submit', onSubmit)
+
 
 function onFeedback(event) {
     event.preventDefault();
-const {email,message} = event.currentTarget.elements;
-const feedback = {
+       
+    const {email,message} = event.currentTarget.elements;
+    const feedback = 
+    {
     email : email.value,
     message : message.value
+    }
+    setLocalStorage(feedback);
+// localStorage.setItem("feedback-form-state",JSON.stringify(feedback))
 }
 
-localStorage.setItem("feedback-form-state",JSON.stringify(feedback))
-
-}
+function setLocalStorage(feedback) {
+    localStorage.setItem("feedback-form-state",JSON.stringify(feedback))
+  }
 
 const result =  JSON.parse(localStorage.getItem("feedback-form-state"))
 
@@ -28,7 +35,6 @@ textareaEl.value = result.message;
 emailEl.value = result.email;
 }
 
-
 function onSubmit(e){
     e.preventDefault();
     console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
@@ -36,6 +42,7 @@ function onSubmit(e){
     emailEl.value = " ";
     localStorage.removeItem("feedback-form-state");   
 };
+
 
 
 
