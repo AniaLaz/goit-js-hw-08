99bf8424d3a88f6cec78bb62257494d783c31030
AniaLaz <104758969+AniaLaz@users.noreply.github.com>
2022-10-01 12:29:50 +0000

    Deploying to gh-pages from @ AniaLaz/goit-js-hw-08@ff28dad59b3a242b4748939c4b42248a079fb984 🚀

--- 03-feedback.b9f1bbe0.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("form");console.log(e),e.addEventListener("input",(function(e){e.preventDefault();const{email:t,message:o}=e.currentTarget.elements,n={email:t.value,message:o.value};console.log(n),localStorage.setItem("feedback-form-state",JSON.stringify(n))}));
-//# sourceMappingURL=03-feedback.b9f1bbe0.js.map
